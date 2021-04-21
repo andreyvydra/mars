4c34d0d482cefea0f35ad206f9b6b9750f5b2883
shmonder-glitch <55046615+shmonder-glitch@users.noreply.github.com>
2021-04-21 21:07:16 +0300

    ADD hazard category

--- data/jobs.py
+++ data/jobs.py
@@ -10,16 +10,18 @@ class Jobs(SqlAlchemyBase, SerializerMixin):
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
     job = sqlalchemy.Column(sqlalchemy.Text)
     work_size = sqlalchemy.Column(sqlalchemy.Integer)
     collaborators = sqlalchemy.Column(sqlalchemy.String)
     start_date = sqlalchemy.Column(sqlalchemy.DateTime)
     end_date = sqlalchemy.Column(sqlalchemy.DateTime)
     is_finished = sqlalchemy.Column(sqlalchemy.Boolean)
+    hazard_category = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("hazards.id"))
 
     user_id = sqlalchemy.Column(sqlalchemy.Integer)
 
     is_published = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
 
     user = orm.relation("User")
+    hazards = orm.relation("Hazards", back_populates="jobs")
